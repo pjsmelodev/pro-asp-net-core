4ef870a19e5a6ace8cccbd9e273ffae1d87f106a
pjsm <pjsm.lesi@gmail.com>
2025-05-06 17:19:02 +0100

    Created products model

---
+++ LanguageFeatures/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+
+        public string Name { get; set; }
+
+        public decimal? Price { get; set; }
+
+        public static Product[] GetProducts()
+        {
+            Product kayak = new Product()
+            {
+                Name = "Kayak",
+                Price = 275M
+            };
+
+            Product lifejack = new Product()
+            {
+                Name = "Lifejack",
+                Price = 48.95M
+            };
+
+            return new Product[] { kayak, lifejack, null };
+        }
+    }
+}
