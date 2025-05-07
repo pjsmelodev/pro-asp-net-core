95679d5c914a5cf445611412e68abe102dc34706
pjsm <pjsm.lesi@gmail.com>
2025-05-07 16:40:28 +0100

    Created shopping cart model

---
+++ LanguageFeatures/Models/ShoppingCart.cs
@@ -0,0 +1,8 @@
+﻿namespace LanguageFeatures.Models
+{
+    public class ShoppingCart
+    {
+        // O Enumerable e os seus produtos são ambos nullable
+        public IEnumerable<Product?>? Products { get; set; }
+    }
+}
