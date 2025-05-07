f5bca603cd0888a4368ccf9879a9059c326b9c76
pjsm <pjsm.lesi@gmail.com>
2025-05-07 16:47:43 +0100

    Created an extension method to the shopping cart class

---
+++ LanguageFeatures/Models/MyExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿namespace LanguageFeatures.Models
+{
+    public static class MyExtensionMethods
+    {
+        // this -> faz com que TotalPrices seja um extension method
+        public static decimal TotalPrices(this ShoppingCart cartParam)
+        {
+            decimal total = 0;
+            if (cartParam.Products != null)
+            {
+                foreach (Product? prod in cartParam.Products)
+                {
+                    total += prod?.Price ?? 0;
+                }
+            }
+            return total;
+        }
+    }
+}
