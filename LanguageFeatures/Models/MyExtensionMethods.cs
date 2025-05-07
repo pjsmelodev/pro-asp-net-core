a5807c70e99b42f020fa29f460f755833423788a
pjsm <pjsm.lesi@gmail.com>
2025-05-07 18:45:12 +0100

    Created filter by name extension method

--- LanguageFeatures/Models/MyExtensionMethods.cs
+++ LanguageFeatures/Models/MyExtensionMethods.cs
@@ -34,5 +34,16 @@
                 }
             }
         }
+
+        public static IEnumerable<Product?> FilterByName(this IEnumerable<Product?> productEnum, char firstLetter)
+        {
+            foreach (Product? prod in productEnum)
+            {
+                if (prod?.Name[0] == firstLetter)
+                {
+                    yield return prod;
+                }
+            }
+        }
     }
 }
