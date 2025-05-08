6e155fc1d6a3134a3419927853d983373a04a082
pjsm <pjsm.lesi@gmail.com>
2025-05-08 17:48:00 +0100

    Updated async methods class with an async enumerable

--- LanguageFeatures/Models/MyAsyncMethods.cs
+++ LanguageFeatures/Models/MyAsyncMethods.cs
@@ -25,5 +25,22 @@ namespace LanguageFeatures.Models
             return httpMessage.Content.Headers.ContentLength;
             // já não precisa de dois returns
         }
+
+        // asynch enumerables describe a sequence of values to be generated over time
+        public static async Task<IEnumerable<long?>>
+            GetPageLengths(List<string> output, params string[] urls)
+        {
+            List<long?> results = new List<long?>();
+            HttpClient cleint = new HttpClient();
+            foreach (string url in urls)
+            {
+                output.Add($"Started request for {url}");
+                var httpMessage = await cleint.GetAsync($"http://{url}");
+                results.Add(httpMessage.Content.Headers.ContentLength);
+                output.Add($"Completed request for {url}");
+            }
+
+            return results;
+        }
     }
 }
