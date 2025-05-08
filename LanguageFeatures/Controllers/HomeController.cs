500db75253de434e068e2e99d229b795cf2cd7f4
pjsm <pjsm.lesi@gmail.com>
2025-05-08 16:19:03 +0100

    Updated home controller with type inference

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -7,7 +7,13 @@
         //    return View(Product.GetProducts().Select(p => p?.Name));
         //}
 
-        public ViewResult Index() => View(Product.GetProducts().Select(p => p?.Name));
+        //public ViewResult Index() => View(Product.GetProducts().Select(p => p?.Name));
         // lambda notation não carece de return (implícito)
+
+        public ViewResult Index()
+        {
+            var names = new[] { "Kayak", "Lifejacket", "Soccer ball" };
+            return View(names);
+        }
     }
 }
