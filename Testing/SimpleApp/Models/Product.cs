c740d36b75d1f9cc2c0ca4f6c3e45edb92615bc7
pjsm <pjsm.lesi@gmail.com>
2025-05-09 17:06:14 +0100

    Created product model class

---
+++ Testing/SimpleApp/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace SimpleApp.Models
+{
+    public class Product
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal? Price { get; set; }
+
+        public static Product[] GetProducts()
+        {
+            Product Kayak = new Product
+            {
+                Name = "Kayak",
+                Price = 275M
+            };
+
+            Product Lifejacket = new Product
+            {
+                Name = "Lifejacket",
+                Price = 48.95M
+            };
+
+            return new Product[] { Kayak, Lifejacket };
+        }
+    }
+}
