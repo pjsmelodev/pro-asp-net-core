c598239982afd05c56c06362c9fffff73eb49b09
pjsm <pjsm.lesi@gmail.com>
2025-05-09 16:44:57 +0100

    Updated home controller to use nameof expressions

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -11,7 +11,11 @@
                 new { Name = "Soccer ball", Price = 19.50M},
                 new { Name = "Corner flag", Price = 39.95M}
             };
-            return View(products.Select(p => $"Name: {p.Name}, Price: {p.Price:C}"));
+            //return View(products.Select(p => $"Name: {p.Name}, Price: {p.Price:C}"));
+
+            // nameof é um método usado para obter o nome de uma variável, propriedade ou tipo como uma string.
+            return View(products.Select(p =>
+                $"{nameof(p.Name)}: {p.Name}, {nameof(p.Price)}: {p.Price}"));
         }
     }
 }
