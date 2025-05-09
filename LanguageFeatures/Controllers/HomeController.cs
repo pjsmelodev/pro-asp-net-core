8600a92fe59b4f31135b3acd6bbb294c14683a44
pjsm <pjsm.lesi@gmail.com>
2025-05-09 16:34:18 +0100

    Fixed home controller and async methods class

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -5,8 +5,9 @@
         public async Task<ViewResult> Index()
         {
             List<string> output = new List<string>();
-            foreach (long? len in await MyAsyncMethods.GetPageLengths(output,
-            "apress.com", "microsoft.com", "amazon.com"))
+            //foreach (long? len in await MyAsyncMethods.GetPageLengths(output,
+            //"apress.com", "microsoft.com", "amazon.com"))
+            await foreach (long? len in MyAsyncMethods.GetPageLengths(output, "apress.com", "microsoft.com", "amazon.com"))
             {
                 output.Add($"Page length: {len}");
             }
--- LanguageFeatures/Models/MyAsyncMethods.cs
+++ LanguageFeatures/Models/MyAsyncMethods.cs
@@ -1,26 +1,25 @@
 ﻿namespace LanguageFeatures.Models
 {
- public class MyAsyncMethods
-{
-    public async static Task<long?> GetPageLength()
+    public class MyAsyncMethods
     {
-        HttpClient client = new HttpClient();
-        var httpMessage = await client.GetAsync("http://apress.com");
-        return httpMessage.Content.Headers.ContentLength;
-    }
-    public static async Task<IEnumerable<long?>>
-    GetPageLengths(List<string> output, params string[] urls)
-    {
-        List<long?> results = new List<long?>();
-        HttpClient client = new HttpClient();
-        foreach (string url in urls)
+        public async static Task<long?> GetPageLength()
         {
-            output.Add($"Started request for {url}");
-            var httpMessage = await client.GetAsync($"http://{url}");
-            results.Add(httpMessage.Content.Headers.ContentLength);
-            output.Add($"Completed request for {url}");
+            HttpClient client = new HttpClient();
+            var httpMessage = await client.GetAsync("http://apress.com");
+            return httpMessage.Content.Headers.ContentLength;
         }
-        return results;
+
+        public static async IAsyncEnumerable<long?>
+         GetPageLengths(List<string> output, params string[] urls)
+        {
+            HttpClient client = new HttpClient();
+            foreach (string url in urls)
+            {
+                output.Add($"Started request for {url}");
+                var httpMessage = await client.GetAsync($"http://{url}");
+                output.Add($"Completed request for {url}");
+                yield return httpMessage.Content.Headers.ContentLength;
+            }
+        }
     }
-}
 }
