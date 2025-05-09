b50baa7cafba2b728aee116b56be80c202783389
pjsm <pjsm.lesi@gmail.com>
2025-05-09 18:41:55 +0100

    Created tests class (deliberate error)

---
+++ Testing/SimpleApp.Tests/ProductTests.cs
@@ -0,0 +1,33 @@
+﻿using SimpleApp.Models;
+using Xunit;
+
+namespace SimpleApp.Tests
+{
+
+    [Fact]
+    // o erro é, para já, intencional
+    public void CanChangeProductName()
+    {
+        // Arrange
+        var p = new Product { Name = "Test", Price = 100M };
+
+        // Act
+        p.Name = "New Name";
+
+        // Assert
+        Assert.Equal("New Name", p.Name);
+    }
+
+    [Fact]
+    public void CanChangeProductPrice()
+    {
+        // Arrange
+        var p = new Product { Name = "Test", Price = 100M };
+
+        // Act
+        p.Price = 200M;
+
+        // Assert
+        Assert.Equal(100M, p.Price);
+    }
+}
