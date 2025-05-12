3e5d1f5602f55bb9187381f27d885fd01c94c678
pjsm <pjsm.lesi@gmail.com>
2025-05-12 10:48:09 +0100

    Updated home controller to use ProductDataSource class

--- Testing/SimpleApp/Controllers/HomeController.cs
+++ Testing/SimpleApp/Controllers/HomeController.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SimpleApp.Models;
-
+using static SimpleApp.Models.Product;
 namespace SimpleApp.Controllers
 {
     public class HomeController : Controller
     {
+        public IDataSource dataSource = new ProductDataSource();
         public ViewResult Index()
         {
-            return View(Product.GetProducts());
+            return View(dataSource.Products);
         }
     }
 }
