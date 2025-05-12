3fd09ac0dd5889b239ec458e1ae4d018662f26ae
pjsm <pjsm.lesi@gmail.com>
2025-05-12 11:14:48 +0100

    Updated home controller tests class to use Moq

--- Testing/SimpleApp.Tests/HomeControllerTests.cs
+++ Testing/SimpleApp.Tests/HomeControllerTests.cs
@@ -1,17 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using SimpleApp.Controllers;
 using SimpleApp.Models;
-using Xunit;
+using Moq;
+
 namespace SimpleApp.Tests
 {
     public class HomeControllerTests
     {
-        class FakeDataSource : IDataSource
-        {
-            public FakeDataSource(Product[] data) => Products = data;
-            public IEnumerable<Product> Products { get; }
-        }
+        //class FakeDataSource : IDataSource
+        //{
+        //    public FakeDataSource(Product[] data) => Products = data;
+        //    public IEnumerable<Product> Products { get; }
+        //}
 
         [Fact]
         public void IndexActionModelIsComplete()
@@ -23,17 +23,24 @@ namespace SimpleApp.Tests
                 new Product { Name = "P2", Price = 120M },
                 new Product { Name = "P3", Price = 110M }
             };
-            IDataSource data = new FakeDataSource(testData);
+            //IDataSource data = new FakeDataSource(testData);
+            var mock = new Mock<IDataSource>();
+            mock.SetupGet(m => m.Products).Returns(testData);
             var controller = new HomeController();
-            controller.dataSource = data;
+            controller.dataSource = mock.Object;
+            //controller.dataSource = data;
 
             // Act
             var model = (controller.Index() as ViewResult)?.ViewData.Model
             as IEnumerable<Product>;
 
             // Assert
-            Assert.Equal(data.Products, model, Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.name
-                && p1?.Price == p2?.Price));
+            //Assert.Equal(data.Products, model, Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.name
+            //    && p1?.Price == p2?.Price));
+            Assert.Equal(testData, model,
+                Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.Name
+                    && p1?.Price == p2?.Price));
+            mock.VerifyGet(m => m.Products, Times.Once);
         }
     }
 }
