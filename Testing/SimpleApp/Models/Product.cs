8ebf881168e0037ce6c2b33896a0105f09d6581a
pjsm <pjsm.lesi@gmail.com>
2025-05-12 10:45:22 +0100

    Removed static method from products; Updated products model creating a class that implements IDataSource interface for testing purposes

--- Testing/SimpleApp/Models/Product.cs
+++ Testing/SimpleApp/Models/Product.cs
@@ -5,21 +5,31 @@
         public string Name { get; set; } = string.Empty;
         public decimal? Price { get; set; }
 
-        public static Product[] GetProducts()
+        //public static Product[] GetProducts()
+        //{
+        //    Product kayak = new Product()
+        //    {
+        //        Name = "Kayak",
+        //        Price = 275M
+        //    };
+
+        //    Product lifejacket = new Product()
+        //    {
+        //        Name = "Lifejacket",
+        //        Price = 48.95M
+        //    };
+
+        //    return new Product[] { kayak, lifejacket };
+        //}
+
+        public class ProductDataSource : IDataSource
         {
-            Product kayak = new Product()
-            {
-                Name = "Kayak",
-                Price = 275M
-            };
-
-            Product lifejacket = new Product()
-            {
-                Name = "Lifejacket",
-                Price = 48.95M
-            };
-
-            return new Product[] { kayak, lifejacket };
+            public IEnumerable<Product> Products =>
+                new Product[]
+                {
+                    new Product { Name = "Kayak", Price = 275M },
+                    new Product { Name = "Lifejacket", Price = 48.95M }
+                };
         }
     }
 }
