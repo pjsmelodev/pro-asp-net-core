34b66b7e8c096a1e8f15adfa7ae3018d6c8542fd
pjsm <pjsm.lesi@gmail.com>
2025-05-16 15:41:48 +0100

    Fixed typo in the navigation menu view

--- SportsSln/SportsStore/Components/NavigationMenuViewComponent.cs
+++ SportsSln/SportsStore/Components/NavigationMenuViewComponent.cs
@@ -1,17 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
-
 namespace SportsStore.Components
 {
     public class NavigationMenuViewComponent : ViewComponent
     {
         private IStoreRepository repository;
-
         public NavigationMenuViewComponent(IStoreRepository repo)
         {
             repository = repo;
         }
-
         public IViewComponentResult Invoke()
         {
             return View(repository.Products
